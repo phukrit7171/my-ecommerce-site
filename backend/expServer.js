7dff3ab252f6bf12c7090a5c2e0b4cce7151b3a2
Phukrit Kittinontana <phukrit7171@gmail.com>
2025-05-09 14:06:16 +0700

    add : init subject in backend/router

--- backend/expServer.js
+++ backend/expServer.js
@@ -10,7 +10,20 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// api route
+/* 
+    Api routes
+*/
+
+//GET api
+app.get('/api', (req, res) => {
+    res.json({ message: 'Hello from the server!' });
+});
+
+//POST api
+app.post('/api', (req, res) => {
+    const { name } = req.body;
+    res.json({ message: `Hello ${name} from the server!` });
+});
 
 
 
